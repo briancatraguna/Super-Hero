c0926771d1801274725b00413feb57ab160147da
unknown <brian.catraguna@aido.id>
2021-10-04 16:40:07 +0700

    Add reactive feature on room

--- core/src/main/java/com/briancatraguna/superhero/core/data/LocalDataSource.kt
+++ core/src/main/java/com/briancatraguna/superhero/core/data/LocalDataSource.kt
@@ -1,11 +1,18 @@
 package com.briancatraguna.superhero.core.data
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import io.reactivex.Observer
 import com.briancatraguna.superhero.core.data.room.HeroDao
 import com.briancatraguna.superhero.core.domain.HeroEntity
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
 import javax.inject.Inject
 
 class LocalDataSource: ILocalDataSource {
+
+    private val _heroes = MutableLiveData<List<HeroEntity>>()
 
     lateinit var heroDao: HeroDao
 
@@ -15,7 +22,9 @@ class LocalDataSource: ILocalDataSource {
     }
 
     override fun getHeroes(): LiveData<List<HeroEntity>> {
-        return heroDao.getAllHeroes()
+        loadData()
+        val heroes: LiveData<List<HeroEntity>> = _heroes
+        return heroes
     }
 
     override fun insertHero(heroEntity: HeroEntity) {
@@ -24,5 +33,33 @@ class LocalDataSource: ILocalDataSource {
 
     override fun deleteHero(heroName: String) {
         heroDao.delete(heroName)
+    }
+
+    private fun loadData(){
+        heroDao.getAllHeroes()
+            .subscribeOn(Schedulers.io())
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe(getHeroesListObserverRx())
+    }
+
+    private fun getHeroesListObserverRx(): Observer<List<HeroEntity>>{
+        return object : Observer<List<HeroEntity>>{
+            override fun onSubscribe(d: Disposable) {
+
+            }
+
+            override fun onNext(t: List<HeroEntity>) {
+                _heroes.value = t
+            }
+
+            override fun onError(e: Throwable) {
+
+            }
+
+            override fun onComplete() {
+
+            }
+
+        }
     }
 }
--- core/src/main/java/com/briancatraguna/superhero/core/data/room/HeroDao.kt
+++ core/src/main/java/com/briancatraguna/superhero/core/data/room/HeroDao.kt
@@ -1,14 +1,14 @@
 package com.briancatraguna.superhero.core.data.room
 
-import androidx.lifecycle.LiveData
 import androidx.room.*
 import com.briancatraguna.superhero.core.domain.HeroEntity
+import io.reactivex.Observable
 
 @Dao
 interface HeroDao {
 
     @Query("SELECT * FROM heroentity")
-    fun getAllHeroes(): LiveData<List<HeroEntity>>
+    fun getAllHeroes(): Observable<List<HeroEntity>>
 
     @Insert(onConflict = OnConflictStrategy.IGNORE)
     fun insert(heroEntity: HeroEntity)
